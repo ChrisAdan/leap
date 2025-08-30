690a8c7c21d27dee8ff2fb9b3cbd4fde94e5601e
Chris Adan <55331613+ChrisAdan@users.noreply.github.com>
2025-08-30 14:42:41 -0400

    Feat: add dbt stage models

--- dbt_project/models/staging/stage_conflicts.sql
+++ dbt_project/models/staging/stage_conflicts.sql
@@ -1,11 +1,11 @@
 {{ config(
     materialized='incremental',
-    unique_key='session_id || team_1_id || team_2_id || encounter_start',
+    unique_key='session_id || team_1_id || team_2_id || conflict_start',
     contract={"enforced": true},
     on_schema_change='fail'
 ) }}
 
-{{ compute_encounters(
+{{ compute_conflicts(
     centroids_table=ref('stage_centroids'),
     distance_threshold=30,
     cooldown_seconds=60
